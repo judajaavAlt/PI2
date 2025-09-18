91345292955aa184009083d9d924bf097f49cbca
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2025-09-18 17:03:26 -0500

    path handler

---
+++ infraestructure/persistence/paths.py
@@ -0,0 +1,17 @@
+import os
+import platform
+
+
+class paths:
+    @classmethod
+    def get_data_dir(cls):
+        system = platform.system()
+        if system == 'Windows':
+            base = os.getenv("APPDATA", "")
+        path = os.path.join(base, "HabitApp")
+        os.makedirs(path, exist_ok=True)
+        return path
+
+    @classmethod
+    def get_db_path(cls):
+        return os.path.join(cls.get_data_dir(), 'database.sqlite')
