7b1ef52091c6d8ad64513a510b60306c8a047c7b
Andres <124212022+AndresIsCoding@users.noreply.github.com>
2025-09-25 20:46:37 -0500

    Second version + test

---
+++ infraestructure/settings/__init__.py
@@ -0,0 +1,6 @@
+from preferences import Preferences
+
+# Create a preferences object
+prefs = Preferences()
+
+print(prefs.get_preferences())
--- infraestructure/settings/preferences.py
+++ infraestructure/settings/preferences.py
@@ -3,20 +3,23 @@ import datetime
 
 # Save the user preferences
 class Preferences:
-    # Initial preferences for the app, this includes username and hour_format (12-hour with AM or PM)
-    preferences_data = {
-       "username" : " ",
-        "hour_format" : "%I %p",
-    } 
+    
+    hour_formats = {
+        "12h": "%I %p",
+        "24h": "%H:%M",
+        "with_seconds": "%H:%M:%S"
+    }
 
     def __init__(self):
-        pass
+        self.preferences_data = {
+            "username" : " ",
+            "hour_format" : self.hour_formats["12h"]} 
         
-    def update_preferences(self,new_name=None,new_hour_format=None):
-        if new_name is not None:
-            self.__class__.preferences_data["username"] = new_name
-        if new_hour_format is not None:
-            self.__class__.preferences_data["edad"] = new_hour_format
+    def update_preferences(self,new_username=None,new_hour_format=None):
+        if new_username is not None:
+            self.__class__.preferences_data["username"] = new_username
+        if new_hour_format in self.hour_formats:
+            self.__class__.preferences_data["hour_format"] = self.hour_formats["new_hour_format"]
 
     def get_preferences(self):
        return self.preferences_data
