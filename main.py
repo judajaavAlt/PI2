993910ac1883765b74f3728f59794a026642507a
Andres <124212022+AndresIsCoding@users.noreply.github.com>
2025-09-29 00:25:13 -0500

    ui added to main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from infraestructure.persistence.habit_repository_sqlite import HabitSqliteRepository
+from infraestructure.ui.ui import UiStarter
+
 
 uses = HabitSqliteRepository()
+UiStarter.start()
