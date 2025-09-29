9f6e66ac1539840edc949949c07bb0bfdc2b4140
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2025-09-28 19:25:04 -0500

    hotfix

--- domain/utils/checks.py
+++ domain/utils/checks.py
@@ -1,5 +1,5 @@
 def check_type(name, value, expected_type):
     if not isinstance(value, expected_type):
-        error_msg = f"{name} must be of type {type.__name__}"
+        error_msg = f"{name} must be of type {expected_type.__name__}"
         error_msg += f", got {type(value).__name__}"
         raise TypeError(error_msg)
