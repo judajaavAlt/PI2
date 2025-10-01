07db0eae5036b3b157db3184ce1fee634f459885
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2025-10-01 12:44:46 -0500

    Update update_streak.py

--- application/use_cases/update_streak.py
+++ application/use_cases/update_streak.py
@@ -1,12 +1,17 @@
 from domain.entities.habit import Habit
 from domain.repositories.habit_repository import HabitRepository
+from infraestructure.persistence.time import TimeManager as tm
 
 
 class UpdateStreak:
     def __init__(self, repository: HabitRepository) -> None:
         self.repository = repository
 
-    def execute(self, habit: Habit, keep_streak: bool):
+    def execute(self, habit: Habit):
+        lost_days = tm.get_lost_days()
+        habit_days = habit.frequency.value
+        keep_streak = sum([lost_days[i] & habit_days[i] for i in range(7)]) == 0
+        
         if keep_streak:
             habit.modify(streak=habit.streak.increase().value)
         else:
