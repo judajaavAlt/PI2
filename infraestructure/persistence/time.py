47e2bd4884436dff8d0e4664fe3c1c475f1895b5
judajaavAlt <132619919+judajaavAlt@users.noreply.github.com>
2025-10-01 12:53:24 -0500

    Update time.py

--- infraestructure/persistence/time.py
+++ infraestructure/persistence/time.py
@@ -26,3 +26,17 @@ class TimeManager:
         d1 = datetime.strptime(t1, fmt).date()
         d2 = datetime.strptime(t2, fmt).date()
         return abs((d2 - d1).days)
+
+    @classmethod
+    def get_lost_days(cls, passed_days):
+        days = passed_days if passed_days <= 7 else 7
+        days_list = {
+        "monday": 0, "tuesday": 1, "wednesday": 2, "thursday": 3,
+        "friday": 4, "saturday": 5, "sunday": 6
+        }
+        today = days_list[cls.today()]
+        days_list = [0, 0, 0, 0, 0, 0, 0]
+        for i in range(days):
+            days_list[today - (i + 1)] = 1
+    
+        return days_list 
